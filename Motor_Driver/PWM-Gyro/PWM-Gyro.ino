8565ea113bbecc7a11c9e46c64b5ad5b7faeaaba
Maico717 <123maikelin123@gmail.com>
2025-03-04 09:00:13 -0800

    changes

--- Motor_Driver/PWM-Gyro/PWM-Gyro.ino
+++ Motor_Driver/PWM-Gyro/PWM-Gyro.ino
@@ -4,12 +4,6 @@
 #include "math.h"
 #include "Arduino_BMI270_BMM150.h"
 
-int pwm_lf_button = 6;                                                // Button to control PWM rate for Motor A
-int pwm_lb_button = 8;                                                // Button to control PWM rate for Motor B
-
-int pwm_rf_button = 10;                                               // Button to control PWM rate for Motor A
-int pwm_rb_button = 12;                                               // Button to control PWM rate for Motor B
-
 float PWM_A = 0;                                                      // Initial PWM value A
 float PWM_B = 0;                                                      // Initial PWM value B
 
@@ -20,35 +14,38 @@ int right_1 = 4;                                                         // PWM 
 int right_2 = 5;                                                         // PWM from digital pin 5
 
 float tm = 1/99.84;
-    float degx= 0;
-    float degy = 0;
-    float degz= 0;
-    float degacc= 0;
-    float degt = 0;
-    float kg= 0.5; //gyroscope weight
-    float ka= 0.5; //accelerometer weight
+float degx= 0;
+float degy = 0;
+float degz= 0;
+float degacc= 0;
+float degt = 0;
+
+//Global Varibles for the PID
+unsigned long previous_T = 0;
+float integral = 0;
+float previous_angle = 0;
+
+unsigned long timer_value = 0;
 
 void setup() {
 
-  pinMode(pwm_lf_button, INPUT_PULLUP);                               // Use INPUT_PULLUP if no external pull-down resistor
-  pinMode(pwm_lb_button, INPUT_PULLUP);                               // Use INPUT_PULLUP if no external pull-down resistor
-  pinMode(pwm_rf_button, INPUT_PULLUP);                               // Use INPUT_PULLUP if no external pull-down resistor
-  pinMode(pwm_rb_button, INPUT_PULLUP);                               // Use INPUT_PULLUP if no external pull-down resistor
+    Serial.begin(9600);                                                 // Initialize serial communication
 
-  Serial.begin(9600);                                                 // Initialize serial communication
+    while (!Serial);
+    Serial.println("Started");
 
-  while (!Serial);
-  Serial.println("Started");
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU!");
+        while (1);
+    }
 
-  if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
-    while (1);
-  }
+    
 }
 
-void loop() {
-    float gyro_angle;
+float check_angle(){
     float x, y, z,ax,ay,az;
+    float kg = 0.5; //gyroscope weight
+    float ka = 0.5; //accelerometer weight
 
     if (IMU.gyroscopeAvailable()) {
         IMU.readGyroscope(x, y, z);
@@ -65,25 +62,58 @@ void loop() {
     
         degt=kg*degx+ka*degacc;
     }
-    gyro_angle = degt;
+    return degt;
+}
 
-    if(gyro_angle > 0){
-        PWM_A = PWM_B = gyro_angle*255.0/90.0;
+int PID(float gyro_angle,float wanted_angle){
+    float Kp = 2.2;
+    float Ki = 10;
+    float Kd = 0.019;
+    int Max_PID = 255;
+
+    float current_T = millis();
+    float dT = current_T - previous_T;
+    previous_T = current_T;
+
+    float error = wanted_angle - gyro_angle;
+
+    integral += error * dT;
+
+    float derivative = (previous_angle-gyro_angle)/dT;
+    previous_angle = gyro_angle;
+
+    int result = (Kp * error) + (Ki * integral) + (Kd * derivative);
+
+    if(result > Max_PID){
+        result = Max_PID;
+    } else if(result < -Max_PID){
+        result = Max_PID;
+    }
+    return result;
+}
+
+void loop() {
+    float gyro_angle;
+    float wanted_angle = 0;
+
+    if(millis()-timer_value>10){
+      timer_value = millis();
+      gyro_angle = check_angle();
+
+      PWM_A = PWM_B = PID(gyro_angle, wanted_angle);
+
+      if(gyro_angle > 0){
         analogWrite(left_1, PWM_A);
         analogWrite(left_2, 0);
         analogWrite(right_1, PWM_B);
         analogWrite(right_2, 0);
-    } else {
-        PWM_A = PWM_B = (-gyro_angle)*255.0/90.0;
+      } else {
         analogWrite(left_1, 0);
         analogWrite(left_2, PWM_A);
         analogWrite(right_1, 0);
         analogWrite(right_2, PWM_B);
+      }
     }
 
-    Serial.print(PWM_A);
-    Serial.print('\t');
-    Serial.print(PWM_B);
-    Serial.print('\t');
-    Serial.println(gyro_angle);
+    
 }
